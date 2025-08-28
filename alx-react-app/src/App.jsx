d1da97002ae1368e9feb69f7cd95625055803ade
osariemen iyoha <iyohapreciousosariemen@gmail.com>
2025-08-28 11:58:04 +0100

    Display Task 1, 2, and 3 components in App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,14 +1,28 @@
 import './App.css';
+
+
 import WelcomeMessage from './components/WelcomeMessage';
+
+
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+
+
 import UserProfile from './components/UserProfile';
 
 function App() {
   return (
     <>
-      {/* Task 1: WelcomeMessage */}
+     
       <WelcomeMessage />
 
-      {/* Task 3: UserProfile */}
+     
+      <Header />
+      <MainContent />
+      <Footer />
+
+    
       <UserProfile 
         name="Alice" 
         age="25" 
