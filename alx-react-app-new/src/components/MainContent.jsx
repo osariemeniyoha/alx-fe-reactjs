7304bdc99cd2c761fd86f7d5c1367994c866d43f
osariemen iyoha <iyohapreciousosariemen@gmail.com>
2025-09-05 13:50:23 +0100

    Modify the paragraph section

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -8,13 +8,11 @@ function MainContent() {
         textAlign: 'center' 
       }}
     >
-      <h2 style={{ color: 'white', marginBottom: '10px' }}>
+      <h2 style={{ color: 'darkslategray', marginBottom: '10px' }}>
         Favorite Cities
       </h2>
       <p style={{ lineHeight: '1.6', fontSize: '16px' }}>
-        I love to visit <span style={{ fontWeight: 'bold' }}>New York</span>, 
-        <span style={{ color: 'crimson' }}> Paris</span>, and 
-        <span style={{ color: 'darkblue' }}> Tokyo</span>.
+        I love to visit New York, Paris, and Tokyo.
       </p>
     </main>
   );
