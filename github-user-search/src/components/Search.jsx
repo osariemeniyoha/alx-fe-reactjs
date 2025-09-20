1b642718fe292d64e98fde0479e524b8e719a440
osariemen iyoha <iyohapreciousosariemen@gmail.com>
2025-09-20 04:36:29 +0100

    Imported fetchUserData alongside fetchAdvancedUsers in Search.jsx

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { fetchAdvancedUsers } from "../services/githubService";
+import { fetchUserData, fetchAdvancedUsers } from "../services/githubService";
 
 function Search() {
   const [username, setUsername] = useState("");
